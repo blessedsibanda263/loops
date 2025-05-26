b200f06a7a8128d6579264c94dabe15505389bd9
Blessed Sibanda <blessedsibanda263@gmail.com>
2025-05-26 06:49:06 +0200

    Conditional Loops with while

--- src/main.rs
+++ src/main.rs
@@ -26,4 +26,11 @@ fn main() {
         count += 1;
     }
     println!("End count = {count}");
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}!");
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
 }
